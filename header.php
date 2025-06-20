db3d9e6c98a8b8ba389fee5c7904b1f6bfe9d5ca
AsoboSally-Ann <asobosallyann13@gmail.com>
2025-06-20 17:55:34 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cameroon Tour Planner</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<header>
+    <h1>Cameroon Tour Planner</h1>
+</header>
+<div class="container">
